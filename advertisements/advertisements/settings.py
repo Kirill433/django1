fb5304251b6d80a9a680219023c308b22c3f319c
kirill <milenin.kiril@mail.ru>
2023-07-28 13:29:22 +0300

    3

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    '234': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db2.PostgeSQL',
     }
 }
 
